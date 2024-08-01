16e36f87fd181652428b9bacd27161248f0ed541
Mawsis <Mawsis448@outlook.com>
2024-08-01 01:26:00 +0100

    switch to kitty

--- dwm/config.h
+++ dwm/config.h
@@ -78,7 +78,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg, "-nf", "#ffffff" , "-sb", main_color, "-sf", "#ffffff", NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 static const char *screenshot[]  = { "spectacle", NULL };
 static const char *browser[]  = { "firefox", NULL };
 static const char *notes[]  = { "obsidian", NULL };
